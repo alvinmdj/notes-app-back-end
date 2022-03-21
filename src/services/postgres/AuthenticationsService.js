af108a253ce3e4504409f532a6671ba13dbb092c
Alvin <alvinmartindwrk@gmail.com>
2022-03-21 22:13:40 +0700

    add refresh token function

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -4,6 +4,23 @@ class AuthenticationsService {
   constructor() {
     this._pool = new Pool();
   }
+
+  async addRefreshToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES ($1)',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async verifyRefreshToken() {
+
+  }
+
+  async deleteRefreshToken() {
+
+  }
 }
 
 module.exports = AuthenticationsService;
