9ae720596c50750582c32aad7a6d5f0d25acf618
Alvin <alvinmartindwrk@gmail.com>
2022-03-22 21:51:59 +0700

    add auth options to notes routes

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -3,26 +3,41 @@ const routes = (handler) => [
     method: 'POST',
     path: '/notes',
     handler: handler.postNoteHandler, // receive and store a single note
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/notes',
     handler: handler.getNotesHandler, // return many notes
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/notes/{id}',
     handler: handler.getNoteByIdHandler, // return a single note
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
     handler: handler.putNoteByIdHandler, // receive and update a single note
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
     handler: handler.deleteNoteByIdHandler, // delete a single note
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
 ];
 
