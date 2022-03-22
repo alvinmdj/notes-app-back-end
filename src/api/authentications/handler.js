4e18abcec07c5e4ebf906bb25b1f97cc5f93ee1f
Alvin <alvinmartindwrk@gmail.com>
2022-03-22 20:33:37 +0700

    create postAuthenticationHandler to authenticate user

---
+++ src/api/authentications/handler.js
@@ -0,0 +1,52 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AuthenticationsHandler {
+  constructor(authenticationsService, usersService, tokenManager, validator) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+  }
+
+  async postAuthenticationHandler(request, h) {
+    try {
+      this._validator.validatePostAuthenticationPayload(request.payload);
+
+      const { username, password } = request.payload;
+      const id = await this._usersService.verifyUserCredential(username, password);
+
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+      const refreshToken = this._tokenManager.generateRefreshToken({ id });
+
+      await this._authenticationsService.addRefreshToken(refreshToken);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Authentication has been added',
+        data: { accessToken, refreshToken },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server error
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = AuthenticationsHandler;
