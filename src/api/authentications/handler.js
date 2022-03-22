6a6a967894ded9686d7cee2f6db6843ea8e42db2
Alvin <alvinmartindwrk@gmail.com>
2022-03-22 20:42:40 +0700

    create putAuthenticationHandler to update expired refresh token

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -47,6 +47,45 @@ class AuthenticationsHandler {
       return response;
     }
   }
+
+  async putAuthenticationHandler(request, h) {
+    try {
+      this._validator.validatePutAuthenticationPayload(request.payload);
+
+      const { refreshToken } = request.payload;
+
+      // verify refresh token is in database
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+
+      // verify refresh token signature
+      const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+      return {
+        status: 'success',
+        message: 'Access Token has been updated',
+        data: { accessToken },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server error
+      const response = h.response({
+        status: 'error',
+        message: 'Internal server error',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
 }
 
 module.exports = AuthenticationsHandler;
