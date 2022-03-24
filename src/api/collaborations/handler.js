5b78532410cc32d74e3bc046999713e3c4042fcc
Alvin <alvinmartindwrk@gmail.com>
2022-03-24 21:03:55 +0700

    create post and delete collaboration handler

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,85 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class CollaborationsHandler {
+  constructor(collaborationsService, notesService, validator) {
+    this._collaborationsService = collaborationsService;
+    this._notesService = notesService;
+    this._validator = validator;
+
+    this.postCollaborationHandler = this.postCollaborationHandler.bind(this);
+    this.deleteCollaborationHandler = this.deleteCollaborationHandler.bind(this);
+  }
+
+  async postCollaborationHandler(request, h) {
+    try {
+      this._validator.validateCollaborationPayload(request.payload);
+      const { id: credentialId } = request.auth.credentials;
+      const { noteId, userId } = request.payload;
+
+      this._notesService.verifyNoteOwner(noteId, credentialId);
+      const collaborationId = await this._collaborationsService.addCollaboration(noteId, userId);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Collaboration has been added',
+        data: { collaborationId },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          error: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server Error
+      const response = h.response({
+        status: 'error',
+        error: 'Internal Server Error',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async deleteCollaborationHandler(request, h) {
+    try {
+      this._validator.validateCollaborationPayload(request.payload);
+      const { id: credentialId } = request.auth.credentials;
+      const { noteId, userId } = request.payload;
+
+      await this._notesService.verifyNoteOwner(noteId, credentialId);
+      await this._collaborationsService.deleteCollaboration(noteId, userId);
+
+      return {
+        status: 'success',
+        message: 'Collaboration has been deleted',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          error: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server Error
+      const response = h.response({
+        status: 'error',
+        error: 'Internal Server Error',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = CollaborationsHandler;
