cced88972ef75064866227befd77aad425fc947a
Alvin <alvinmartindwrk@gmail.com>
2022-03-31 21:21:09 +0700

    create exports handler

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -4,12 +4,27 @@ class ExportsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
+
+    this.postExportNotesHandler = this.postExportNotesHandler.bind(this);
   }
 
   async postExportNotesHandler(request, h) {
     try {
       this._validator.validateExportNotesPayload(request.payload);
-      
+
+      const message = {
+        userId: request.auth.credentials.id,
+        targetEmail: request.payload.targetEmail,
+      };
+
+      await this._service.sendMessage('export:notes', JSON.stringify(message));
+
+      const response = h.response({
+        status: 'success',
+        message: 'Your request is in queue',
+      });
+      response.code(201);
+      return response;
     } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
